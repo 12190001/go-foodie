4e2a819d3d03203485f312b97a1e62ef88253423
masterinapi <122201230+masterinapi@users.noreply.github.com>
2023-05-22 18:49:22 +0600

    Update asgi.py

--- final_fyp/final_fyp/asgi.py
+++ final_fyp/final_fyp/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from ./../food.routing import websocket_urlpatterns
+from food.routing import websocket_urlpatterns
 from channels.auth import AuthMiddleware, AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_fyp.settings')
 
